705d8782f08639d74bfb1a246845ab120e293285
Jose Arias Melo <36265049+joceduardoarias@users.noreply.github.com>
2018-11-05 12:19:21 -0300

    modificado sort
    falta guardar en csv y falta guardar en binario.

---
+++ Win_64/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+#define ESC 27
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+
+
+int main()
+{
+    char option = 's';
+    LinkedList* listaEmpleados = ll_newLinkedList();
+
+    do{
+        system("cls");
+            menu();
+        //printf("\n Ingrese option:");
+        //scanf("%d",&option);
+        switch(option)
+        {
+            case '1':
+                system("cls");
+                controller_loadFromText("data.csv",listaEmpleados);
+                system("pause");
+                break;
+            case '3':
+               controller_addEmployee(listaEmpleados);
+                break;
+            case '4':
+                controller_editEmployee(listaEmpleados);
+                break;
+            case '5':
+                controller_removeEmployee(listaEmpleados);
+                break;
+            case '6':
+                system("cls");
+                controller_ListEmployee(listaEmpleados);
+                system("pause");
+                break;
+            case '7':
+                controller_sortEmployee(listaEmpleados);
+                break;
+        }
+    }while((option = getch())!= ESC);
+    return 0;
+}
